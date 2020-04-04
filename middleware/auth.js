913b03ec90c1ac9ece7c4de82af3b421ad6cb89b
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-03 22:42:35 -0700

    added middleware to get token from header, check if there is no token, or to verify if there is. to decode then set token as req.user token then next();

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,24 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
+
+module.exports = function(req, res, next) {
+ //Get token from the header
+ const token = req.header('x-auth-token');
+
+ //Check if there is no token
+ if(!token) {
+   return res.status(401).json({ msg: 'No token found. Not authorized'}) 
+ }
+
+ //Verify token if there is
+ try { 
+  const decoded = jwt.verify(token, config.get('jwtSecret'))
+
+  //Set decoded token as the new token for the req.user
+  req.user = decoded.user;
+  next();
+ } catch(err) {
+  res.status(401).json({ msg: 'Token is not valid'})
+ }
+}
