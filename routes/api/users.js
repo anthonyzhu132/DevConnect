a960711530b92691cd855da62660419534485e7b
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-03 17:56:34 -0700

    added validation for username, email & password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const { check, validationResult } = require('express-validator/check');
 
 // post api/users
 // REGISTER USER
 // PUBLIC access
-router.post('/', (req, res) => {
+router.post('/', [
+  //Running the checks with express-validator
+  check('name', 'Name is required to sign in')
+    .not()
+    .isEmpty(),
+  check('email', 'Please enter valid email').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters')
+    .isLength({ min: 6})
+], (req, res) => {
   console.log(req.body)
   res.send('user route')
 })
