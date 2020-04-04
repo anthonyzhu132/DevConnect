2bb0b9dbd6510ee611b6417eff5c134db82b69c8
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-03 22:54:54 -0700

    added user authentication using middleware that was made to verify users through /api/auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../../middleware/auth');
+const User = require('../../models/user');
 
 // GET api/auth
 // PUBLIC access
-router.get('/', (req, res) => {
-  res.send('auth route')
+router.get('/', auth, async (req, res) => {
+  try { 
+    //Find user by ID that was set by the middleware, sleect everything minus the password
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error, please check')
+  }
+
 })
 
 module.exports = router;
