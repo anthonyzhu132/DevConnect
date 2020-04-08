36af6f35bd25a966edef3f260bdca157f42867c0
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-08 15:18:04 -0700

    added GET user profile endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,10 +8,16 @@ const User = require('../../models/user');
 // GET api/profile/me
 // GET current user's profile
 // Prrivate gateway for specific user
-router.get('/', auth, async (req, res) => {
+router.get('/me', auth, async (req, res) => {
   try {
     //Set the profile to the user id that comes in when the GET request is made with the token
-    const profile = await Profile.findOne({ user: req.user.id })
+    const profile = await Profile.findOne({ user: req.user.id }).populate('user',
+    ['name', 'avatar']);
+
+    if(!profile) {
+      return res.status(400).json({ msg: 'There is no profile for selected user'})
+    }
+
   } catch(err) {
     console.error(err.message);
     res.status(500).send('Server Error');
