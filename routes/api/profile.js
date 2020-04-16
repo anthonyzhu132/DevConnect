3fa5c8c56fd2d9233a0916c5c331a0b96a04c0ec
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-15 21:25:07 -0700

    finished route to delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -222,7 +222,11 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     //Getting remove index
     const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
     
-    
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
   } catch (err) {
     console.error(err.message);
     res.status(400).send('Server Error')
