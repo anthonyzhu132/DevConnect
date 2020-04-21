8257ebcec6b324998bb5e65b4068312936cccc33
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-20 22:03:08 -0700

    added new post route for new posts, included post, user and profile models into posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
+const Post = require('../../models/post');
+const Profile = require('../../models/profile');
+const User = require('../../models/user');
 
-// GET api/auth
-// PUBLIC access
-router.get('/', (req, res) => {
-  res.send('posts route')
+// POST api/posts
+// Create a post
+// Private
+router.post('/', [ auth, [
+  check('text', 'Text is required').not().isEmpty()
+] ],
+async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const user = await User.findById(req.user.id.select('-password'));
+
+  const newPost = {
+     text: req.body.text,
+     name: user.name,
+     avatar: user.avatar,
+     user: req.user.id
+  }
+
 })
 
 module.exports = router;
