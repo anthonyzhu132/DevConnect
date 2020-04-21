87357fc5d2ed3f08b332cf8f47e974a47f02658e
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-21 16:16:10 -0700

    added GET method to GET all posts with a correct auth token

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,15 +10,7 @@ const User = require('../../models/user');
 // POST api/posts
 // Create a post
 // Private
-router.post(
-  '/',
-  [
-    auth,
-    [
-      check('text', 'Text is required')
-        .not()
-        .isEmpty()
-    ]
+router.post('/', [ auth, [check('text', 'Text is required').not().isEmpty()]
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -45,5 +37,22 @@ router.post(
     }
   }
 );
+
+
+// GET api/posts
+// Get all posts
+// Private
+
+router.get('/', auth, async (req, res) => {
+try {
+  const posts = await Post.find().sort({ date: -1 })
+  res.json(posts);
+} catch (err) {
+  console.error(err.message);
+  res.status(500).send('Server Error');
+}
+
+})
+
 
 module.exports = router;
