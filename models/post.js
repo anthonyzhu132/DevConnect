6b15e2287310f460883bb2e9acbf07c0f0385713
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-20 21:39:01 -0700

    finished post schema

--- models/post.js
+++ models/post.js
@@ -6,5 +6,45 @@ const PostSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'users'
   },
-  
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        defualt: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
 })
