511786ea38a59f86c41879751c97655b7ed75099
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-20 22:16:54 -0700

    refactored to show the correct error message when there is no body

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,35 +10,40 @@ const User = require('../../models/user');
 // POST api/posts
 // Create a post
 // Private
-router.post('/', [ auth, [
-  check('text', 'Text is required').not().isEmpty()
-] ],
-async (req, res) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
-
-  try {
-    
-  const user = await User.findById(req.user.id.select('-password'));
-
-  const newPost = {
-     text: req.body.text,
-     name: user.name,
-     avatar: user.avatar,
-     user: req.user.id
-  }
-
-  const post = await newPost.save();
-
-  res.json(post);
-  
-  } catch (err) {
-    console.error(err.message);
-    req.status(500).send('Server Error');
-  }
-
-})
+);
 
 module.exports = router;
