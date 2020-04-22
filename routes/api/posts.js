af46de15efc884c0d9856d883d42a4633da487ca
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-21 17:40:21 -0700

    refactored

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -158,15 +158,12 @@ router.post('/comment/:id', [ auth, [check('text', 'Text is required').not().isE
 ], async (req, res) => {
   const errors = validationResult(req);
 
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
   try {
-    //assigning user and returning minus the password
     const user = await User.findById(req.user.id).select('-password');
-
-    //finding the post by ID
     const post = await Post.findById(req.params.id);
 
     const newComment = {
@@ -179,15 +176,13 @@ router.post('/comment/:id', [ auth, [check('text', 'Text is required').not().isE
     post.comments.unshift(newComment);
 
     await post.save();
-    
-    res.json(post);
 
+    res.json(post.comments);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
 
 });
-
 
 module.exports = router;
