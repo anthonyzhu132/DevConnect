75452acebc2e2359c24e3270cb8388fa902d6e08
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-04-23 02:34:04 -0700

    added onSubmit function to log submittions with form data

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -11,16 +11,26 @@ const Register = () => {
   const { name, email, password, password2 } = formData;
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+  
+  const onSubmit = e => {
+    e.preventDefault();
+
+    if(password !== password2) {
+      console.log('Passwords do not match')
+    } else {
+      console.log(formData);
+    }
+  }
 
   return <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-      <form className="form" action="create-profile.html">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -32,6 +42,7 @@ const Register = () => {
             placeholder="Password"
             name="password"
             minLength="6"
+            value={password} onChange={e => onChange(e)}
           />
         </div>
         <div className="form-group">
@@ -40,6 +51,7 @@ const Register = () => {
             placeholder="Confirm Password"
             name="password2"
             minLength="6"
+            value={password2} onChange={e => onChange(e)}
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
