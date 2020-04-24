0961aa93569b78d9bee7171094769c3f2ffe31bd
Anthony Zhu <anthonyzhu132@gmail.com>
2020-04-24 01:32:25 -0700

    set up SET_ALERT action with reducer

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,7 +1,15 @@
 const initialState = [];
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 export default function(state = initialState, action) {
-    switch(action.type) {
-        case ''
+    const { type, payload } = action;
+
+    switch(type) {
+        case SET_ALERT:
+            return [...state, payload];
+        case REMOVE_ALERT:
+            return state.filter(alert => alert.id !== payload);
+        default:
+            return state;
     }
 }
