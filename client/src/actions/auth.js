6a38ec20a3701a7446c5ad9cf50fa7ab58602066
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-06-03 22:28:23 -0700

    fixed bugs with auth.js in actions

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,14 +4,17 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
-
-//Load User
+// Load User
 export const loadUser = () => async dispatch => {
-  if(localStorage.token) {
+  if (localStorage.token) {
     setAuthToken(localStorage.token);
   }
 
@@ -22,13 +25,12 @@ export const loadUser = () => async dispatch => {
       type: USER_LOADED,
       payload: res.data
     });
-
   } catch (err) {
     dispatch({
       type: AUTH_ERROR
-    })
+    });
   }
-}
+};
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
@@ -36,30 +38,31 @@ export const register = ({ name, email, password }) => async dispatch => {
     headers: {
       'Content-Type': 'application/json'
     }
-  }
+  };
 
   const body = JSON.stringify({ name, email, password });
 
   try {
-    const res = await axios.post('/api/users', body, config)
+    const res = await axios.post('/api/users', body, config);
 
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data
     });
+
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
-    if(errors) {
-      errors.forEach(error => dispatch(setAlert(error.message, 'danger')));
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
     }
-    
+
     dispatch({
       type: REGISTER_FAIL
     });
   }
-
-}
+};
 
 // Login User
 export const login = (email, password) => async dispatch => {
@@ -67,27 +70,34 @@ export const login = (email, password) => async dispatch => {
     headers: {
       'Content-Type': 'application/json'
     }
-  }
+  };
 
   const body = JSON.stringify({ email, password });
 
   try {
-    const res = await axios.post('/api/auth', body, config)
+    const res = await axios.post('/api/auth', body, config);
 
     dispatch({
       type: LOGIN_SUCCESS,
       payload: res.data
     });
+
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
-    if(errors) {
-      errors.forEach(error => dispatch(setAlert(error.message, 'danger')));
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
     }
-    
+
     dispatch({
       type: LOGIN_FAIL
     });
   }
+};
 
-}
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+  dispatch({ type: LOGOUT });
+};
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState, onChange } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
@@ -14,7 +17,7 @@ const Login = () => {
   //OnSubmit when user clicks submit on form 
   const onSubmit = async e => {
     e.preventDefault();
-    console.log('SUCCESS')
+    login(email, password);
   }
 
   return <Fragment>
@@ -41,4 +44,9 @@ const Login = () => {
   </Fragment>
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired
+};
+
+
+export default connect(null, { login })(Login);
